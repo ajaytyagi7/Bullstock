7ebee058a9497affdb645af2bafd202eacca61e7
Ajay <ajaytyagi9120@gmail.com>
2024-11-22 12:30:42 +0530

    update

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path:'./Backend/.env'});
+require('dotenv').config({path:'./.env'});
 
 
 
 const uri = process.env.MONGO_URI;
 console.log(uri)
 if (!uri) {
-    throw new Error('Mongo URI is missing')
+    throw new Error('Mongo URI is missing',uri)
 }
 
 mongoose.connect(uri)
