9d5c3a85db44ae6ade04459549a7913abee73581
Ajay <ajaytyagi9120@gmail.com>
2024-11-22 10:48:56 +0530

    update

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
+console.log('MongoDB URI:', uri);
 
 if (!uri) {
-    throw new Error('Mongo URI is missing')
+    throw new Error('Mongo URI is missing');
 }
 
-mongoose.connect(uri)
-.then((result) => {
-    console.log('Connected to mongoose')
-    
-}).catch((err) => {
-    console.error(err)
-    
-});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        console.log('Connected to mongoose');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
-module.exports=mongoose
+module.exports = mongoose;
