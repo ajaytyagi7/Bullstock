b409e01ec1ac31f0f611f2a68ac1a24404d4c26f
Ajay <ajaytyagi9120@gmail.com>
2024-11-27 17:07:17 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 
 const app=express();
-const port = process.env.PORT || 5012;
+const port = process.env.PORT || 5013;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
